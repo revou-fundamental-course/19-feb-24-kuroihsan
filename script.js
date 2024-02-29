275a109a2739484fadd47ee80d234599f4f36bbf
kuroihsan <160500794+kuroihsan@users.noreply.github.com>
2024-02-29 12:43:56 +0700

    upload urang lanjut yang kemarin

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ document.getElementById("name").innerHTML = name;
 }
 
 replaceName();
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+   showDivs(slideIndex += n); 
+}
+
+function showDivs(n) {
+    var i;
+    var vdoList = document.getElementsByClassName("vdo-slideshow");
+    if (n > vdoList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = vdoList.length;
+
+    for(i =0; i < vdoList.length; i++) {
+        vdoList[i].style.display ="none";
+    }
+
+}
